082e4baa20bf45fbfda4fbf9236a23e76214a24d
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2020-09-30 19:49:25 -0500

    Create rutas.js

---
+++ server/rutas.js
@@ -0,0 +1,70 @@
+const Router = require('express').Router();
+const Users = require('./model.js')
+
+//Obtener todos los usuarios
+Router.get('/all', function(req, res) {
+    Users.find({}).exec(function(err, docs) {
+        if (err) {
+            res.status(500)
+            res.json(err)
+        }
+        res.json(docs)
+    })
+})
+
+// Obtener un usuario por su id
+Router.get('/', function(req, res) {
+    let nombre = req.query.nombre
+    Users.findOne({nombres: nombre}).exec(function(err, doc){
+        if (err) {
+            res.status(500)
+            res.json(err)
+        }
+        res.json(doc)
+    })
+})
+
+// Agregar a un usuario
+Router.post('/new', function(req, res) {
+    let user = new Users({
+        userId: Math.floor(Math.random() * 50),
+        nombres: req.body.nombres,
+        apellidos: req.body.apellidos,
+        email: req.body.email,
+        fechaNacimiento: req.body.fechaNacimiento,
+        psw: req.body.psw
+    })
+    user.save(function(error) {
+        if (error) {
+            res.status(500)
+            res.json(error)
+        }
+        res.send("Registro guardado")
+    })
+})
+
+// Eliminar un usuario por su id
+Router.get('/delete/:id', function(req, res) {
+    let uid = req.params.id
+    Users.remove({userId: uid}, function(error) {
+        if(error) {
+            res.status(500)
+            res.json(error)
+        }
+        res.send("Registro eliminado")
+    })
+})
+
+// Inactivar un usuario por su id
+Router.post('/inactive/:id', function(req, res) {
+
+})
+
+// Activar un usuario por su id
+Router.post('/active/:id', function(req, res) {
+
+})
+
+module.exports = Router
+
+
