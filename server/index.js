1b277ff7062d745858dbb3dd57c0cb30da3437ca
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2020-10-02 23:24:30 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,3 @@
-
-/*
-//CONEXION A MONGOOSE
-
-var MongoClient = require('mongodb').MongoClient
-//nombre de la base de datos nodeDriver
-var url = "mongodb://localhost/nodeDriver"
-
-var mongoose = require('mongoose')
-var Operaciones = require('./CRUD.js')
-
-mongoose.connect(url)
-*/
-
 const http = require('http'),
       path = require('path'),
       Routing = require('./rutas.js'),
@@ -25,7 +11,6 @@ const app = express()
 const Server = http.createServer(app)
 
 mongoose.connect('mongodb://localhost/Agenda')
-
 
 app.use(express.static('client'))
 app.use(bodyParser.json())
