f9208344a0a751f3990acfb40ebf3b01e36b11fa
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2020-10-03 22:27:14 -0500

    Ingeso de img

--- server/model.js
+++ server/model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema
+const SchemaU = mongoose.Schema
 
-let UserSchema = new Schema({
+let UserSchema = new SchemaU({
   userId: { type: Number, required: true, unique: true},
   nombres: { type: String, required: true },
   apellidos: { type: String, required: true},
@@ -13,4 +13,22 @@ let UserSchema = new Schema({
 
 let UserModel = mongoose.model('Usuarios', UserSchema)
 
+const SchemaE = mongoose.Schema
+
+let EventSchema = new SchemaE({
+  eventId: { type: Number, required: true, unique: true},
+  titulo: { type: String, required: true },
+  fechaInicio: { type: Date, required: true},
+  horaInicio: { type: String, required: true},
+  fechaFinalizacion: { type: Date, required: true},
+  horaFinalizacion: { type: String, required: true},
+  diaCompleto: { type: Boolean, required: true},
+  psw: { type: String, required: true},
+  fx_usuario: { type: Number, required: true, unique: true}
+})
+
+let EventModel = mongoose.model('Eventos', EventSchema)
+
 module.exports = UserModel
+module.exports = EventModel
+
