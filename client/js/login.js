b68f9b34c5bf7d6a6a56d58cd270f960d03df196
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2020-10-03 21:14:50 -0500

    Update login.js

--- client/js/login.js
+++ client/js/login.js
@@ -1,6 +1,5 @@
 
 $('.loginButton').on('click', function(event) {
-    alert("login");
     
     var nombreUsuario = $('#user'),
         pass = $('#pass')
@@ -8,8 +7,9 @@ $('.loginButton').on('click', function(event) {
     if (nombreUsuario.val() != "" && pass.val() != "") {
         $.post('users/login',{email: $('#user').val(), psw: $('#pass').val()}, function(response) {
             if (response == "Validado") {
-                //window.location.href = "http://localhost:3000/main.html"
                 window.location.href = "http://localhost:8082/main.html"
+            }else{
+                alert(response);
             }
         })
     } else {
