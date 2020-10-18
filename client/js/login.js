1774d77dbeb64d66c2c824cf15987e0668310c1f
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2020-10-17 21:45:39 -0500

    Update login.js

--- client/js/login.js
+++ client/js/login.js
@@ -7,6 +7,15 @@ $('#login').on('click', function(event) {
     if (nombreUsuario.val() != "" && pass.val() != "") {
         $.post('users/login',{usuario: $('#user').val(), psw: $('#pass').val()}, function(response) {
             if (response == "Validado") {
+                // Check browser support
+                    //if (typeof(Storage) !== "undefined") {
+                        // Store
+                    localStorage.setItem("sesion", nombreUsuario.val());
+                        // Retrieve
+                        //document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+                    //} else {
+                      //  document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+                    //}
                 window.location.href = "http://localhost:8082/main.html"
             }else{
                 alert(response);
@@ -16,5 +25,3 @@ $('#login').on('click', function(event) {
         alert("Complete todos los campos")
     }
 })
-
-  
